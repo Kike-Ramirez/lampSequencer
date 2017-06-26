8de229aecc94f9de3be852947f844b07baefc776
enrique <kike@vjspain.com>
2017-06-26 21:58:07 +0200

    New changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
+
 var http = require('http').Server(app);
+
 var io = require('socket.io')(http);
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
@@ -8,8 +11,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('lampSequencer', function(msg){
+    io.emit('lampSequencer', msg);
   });
 });
 
